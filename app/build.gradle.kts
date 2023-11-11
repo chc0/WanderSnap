537ab3209c0bf77254b13a399c1e7af1dedc3f86
Sergio Cortez <sergiocortez398@gmail.com>
2023-11-11 00:24:41 -0600

    Muestra datos del usuario

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     implementation ("com.google.firebase:firebase-auth")
     implementation ("com.google.firebase:firebase-storage")
     implementation("com.google.firebase:firebase-analytics")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/als/wandersnap/ui/usuario/datosUsuario.java
+++ app/src/main/java/com/als/wandersnap/ui/usuario/datosUsuario.java
@@ -8,13 +8,30 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.als.wandersnap.R;
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class datosUsuario extends Fragment {
+
+    private ImageView ivFotoPerfil;
+    private TextView tvUsuario, tvEmail, tvNombre;
+    private Button btnEditar;
+
+    private String currentImageURL = "";
 
     private DatosUsuarioViewModel mViewModel;
 
@@ -25,7 +42,95 @@ public class datosUsuario extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_datos_usuario, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_datos_usuario, container, false);
+
+        ivFotoPerfil = view.findViewById(R.id.perFotoPerfil);
+        tvUsuario = view.findViewById(R.id.perUsuario);
+        tvEmail = view.findViewById(R.id.perEmail);
+        tvNombre = view.findViewById(R.id.perNombre);
+        btnEditar = view.findViewById(R.id.perbtnEditar);
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (currentUser != null) {
+            // El usuario está autenticado, establece la información en los elementos de la interfaz de usuario
+            String username = currentUser.getDisplayName();
+            String email = currentUser.getEmail();
+
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            Query nameQuery = db.collection("Usuarios").whereEqualTo("email", email);
+
+            nameQuery.get().addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    QuerySnapshot querySnapshot = task.getResult();
+                    if (querySnapshot != null && !querySnapshot.isEmpty()) {
+                        // Supongo que el correo electrónico es único en tu colección
+                        DocumentSnapshot document = querySnapshot.getDocuments().get(0);
+
+                        // Obtén el nombre del usuario
+                        String fullName = document.getString("nombre");
+
+                        // Ahora puedes usar fullName como desees
+                        // Por ejemplo, establecer el texto en un TextView
+                        tvNombre.setText(fullName);
+                    }
+                } else {
+                    // Manejar el error al obtener datos de Firestore
+                }
+            });
+
+            Query userQuery = db.collection("Usuarios").whereEqualTo("email", email);
+
+            userQuery.get().addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    QuerySnapshot querySnapshot = task.getResult();
+                    if (querySnapshot != null && !querySnapshot.isEmpty()) {
+                        // Supongo que el correo electrónico es único en tu colección
+                        DocumentSnapshot document = querySnapshot.getDocuments().get(0);
+
+                        // Obtén el nombre del usuario
+                        String user = document.getString("usuario");
+
+                        // Ahora puedes usar fullName como desees
+                        // Por ejemplo, establecer el texto en un TextView
+                        tvUsuario.setText(user);
+                    }
+                } else {
+                    // Manejar el error al obtener datos de Firestore
+                }
+            });
+
+            Query imgQuery = db.collection("Usuarios").whereEqualTo("email", email);
+
+            imgQuery.get().addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    QuerySnapshot querySnapshot = task.getResult();
+                    if (querySnapshot != null && !querySnapshot.isEmpty()) {
+                        // Supongo que el correo electrónico es único en tu colección
+                        DocumentSnapshot document = querySnapshot.getDocuments().get(0);
+
+                        // Obtén el nombre del usuario
+                        String imagenPath = document.getString("foto_perfil");
+
+                        if (imagenPath != null) {
+                            // Cargar la imagen utilizando Glide o la biblioteca que prefieras
+                            Glide.with(requireContext())
+                                    .load(imagenPath) // Utiliza la URL de descarga de la imagen
+                                    .into(ivFotoPerfil);
+                            currentImageURL = imagenPath;
+                        } else {
+                            ivFotoPerfil.setImageResource(R.drawable.usuario_default);}
+                    }
+                } else {
+                    // Manejar el error al obtener datos de Firestore
+                }
+            });
+
+            tvEmail.setText(email);
+        }
+
+        return view;
     }
 
     @Override
